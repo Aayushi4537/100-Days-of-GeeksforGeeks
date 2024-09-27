63c4bad610ece9049ede4672e5bdd14a99082010
Aayushi4537 <99946240+Aayushi4537@users.noreply.github.com>
2024-09-27 15:21:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square root of a number/square-root-of-a-number.cpp
+++ Difficulty: Easy/Square root of a number/square-root-of-a-number.cpp
@@ -10,11 +10,27 @@ class Solution {
   public:
     long long int floorSqrt(long long int n) {
         // Your code goes here
-        if(n==1) return 1;
-        for(long long i = 1;i<=n/2;i++)
-        {
-            if(i*i<=n && (i+1) *(i+1)>n) return i;
+        int start=1;
+        int end= n;
+        long long mid= start+(end-start)/2;
+        long long ans=1;
+        
+        while(start<=end){
+            long long mid= start+(end-start)/2;
+            if(  mid*mid == n)return mid;
+            else if( mid* mid >n){
+                end=mid-1;
+            }
+            else{
+                ans= mid;
+                start=mid+1;
+                
+            }
+            
+            
+            
         }
+        return ans;
     }
 };
 
